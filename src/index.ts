267014c76473abb1b53f295f7132f7fa2b14a12d
lbw <574524020@qq.com>
2022-04-21 18:39:06 +0800

    feat:basic plugin structure

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as VueCompiler from '@vue/compiler-sfc'
+import { transformSync } from '@swc/core'
+import postHtml from 'posthtml'
+import { ScriptPlugin } from './plugins/script'
+export async function start(sourceCode: string) {
+  let result = sourceCode
+  // 匹配模板内容
+  const templateRegexp = /(?<=^<template>)([\s\S]*)(?=<\/template>)/g
+
+  // 匹配script内容
+  const scriptRegexp = /(?<=(<script).*?>)([\s\S]*)(?=<\/script>)/g
+  try {
+    const descriptor = VueCompiler.parse(sourceCode).descriptor
+    const scriptCode
+      = descriptor.scriptSetup?.content ?? descriptor.script?.content
+    const templateCode = descriptor.template?.content
+    if (templateCode) {
+      // TODO:transform template
+      const { html: templateOut } = await postHtml().use((tree) => {
+        tree.walk((node) => {
+          return node
+        })
+      }).process(templateCode)
+      result = result.replace(templateRegexp, templateOut)
+    }
+    if (scriptCode) {
+      const scriptOut = transformSync(scriptCode, {
+        plugin: ScriptPlugin,
+      })
+      result = result.replace(scriptRegexp, scriptOut.code)
+    }
+  }
+  catch (error) {
+    if (error instanceof Error)
+      throw new Error(error.message)
+    else if (typeof error === 'string')
+      throw new Error(error)
+  }
+  return result
+}
+
+export default function Vue3I18n() {
+  return {
+    name: 'vite-plugin-vue3-i18n',
+    async transform(sourceCode: string, id: string) {
+      if (id.endsWith('.vue')) {
+        const result = start(sourceCode)
+        return {
+          code: result,
+        }
+      }
+    },
+  }
+}
