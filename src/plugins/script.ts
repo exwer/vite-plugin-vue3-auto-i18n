83d8b66b1a749acd26af05ed247f8420108e9e5d
lbw <574524020@qq.com>
2022-04-21 18:52:47 +0800

    chore:tidy up script plugin idea

--- src/plugins/script.ts
+++ src/plugins/script.ts
@@ -2,10 +2,23 @@
 import type { Program } from '@swc/core'
 import { Visitor } from '@swc/core/Visitor.js'
 
+/*
+1. import { useI18n } from 'vue-i18n'
+  - Judging whether statement is repeated
+  - at top of setup function
+2. const { t } = useI18n()
+  - Judging whether statement is repeated
+  - below import statement of useI18n
+3. ref(s) => ref(t('xxx'))
+  - if theres no 'ref' imported,import ref
+4. s => computed(()=>t('xxx'))
+  - if theres no 'computed' imported,import computed
+*/
 class Script extends Visitor {
   // TODO:transform script
   visitProgram(n: Program): Program {
     return n
   }
 }
-export const ScriptPlugin = (module: Program) => new Script().visitProgram(module)
+export const ScriptPlugin = (module: Program) =>
+  new Script().visitProgram(module)
