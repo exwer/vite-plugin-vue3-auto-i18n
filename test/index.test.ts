be7cd1cf7d5a3fcaddd29995518dc13ff76e21ce
lbw <574524020@qq.com>
2022-04-22 17:58:05 +0800

    chroe:Update test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,46 +1,34 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { describe, expect, it } from 'vitest'
-import * as VueCompiler from '@vue/compiler-sfc'
+import { describe, expect, test } from 'vitest'
 import { start } from '../src/index'
-function compile(path: string, target: string) {
-  try {
-    let result = ''
-    const source = fs.readFileSync(
-      path,
-      'utf-8',
-    )
-    const { descriptor } = VueCompiler.parse(source)
-    const { content: script } = VueCompiler.compileScript(descriptor, {
-      id: '123',
-    })
-    const { code: template } = VueCompiler.compileTemplate({
-      source: descriptor.template!.content.toString(),
-      filename: 'fuck',
-      id: '456',
-    })
-    result += script
-    result += template
-    fs.writeFileSync(target, result)
-  }
-  catch (error) {
-    console.error(error)
-  }
-}
+
 describe('import test', () => {
-  it('single script', async() => {
+  test('All absence', async() => {
     const source = '<script>import { a } from "b"</script>'
     const result = await start(source)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchInlineSnapshot(`
+      "<script>import { ref, computed } from \\"vue\\";
+      import { useI18n } from \\"vue-i18n\\";
+      import { a } from \\"b\\";</script>"
+    `)
   })
-  it('complete vue file', async() => {
-    const source = '<template><div></div></template><script></script><style></style>'
+
+  test('no ref', async() => {
+    const source
+      = '<script>import {computed} from "vue";import {useI18n} from "vue-i18n"</script>'
     const result = await start(source)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchInlineSnapshot(`
+      "<script>import { computed, ref } from \\"vue\\";
+      import { useI18n } from \\"vue-i18n\\";</script>"
+    `)
   })
-  it('repeat import', async() => {
-    const source = '<script>import {ref} from "vue";import {useI18n} from "vue-i18n"</script>'
+
+  test('no computed', async() => {
+    const source
+      = '<script>import {ref} from "vue";import {useI18n} from "vue-i18n"</script>'
     const result = await start(source)
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchInlineSnapshot(`
+      "<script>import { ref, computed } from \\"vue\\";
+      import { useI18n } from \\"vue-i18n\\";</script>"
+    `)
   })
 })
