0faf22f55653c3e8ad8e58635609b02691ce097f
lbw <574524020@qq.com>
2022-04-22 11:22:38 +0800

    feat:import ast

--- src/plugins/script.ts
+++ src/plugins/script.ts
@@ -1,54 +1,99 @@
-import type { Program } from '@swc/core'
-import { Visitor } from '@swc/core/Visitor.js'
+import type babelCore from '@babel/core'
 
+interface VisitorState {
+  file: {
+    opts: babelCore.TransformOptions
+  }
+}
 /*
-1. import { useI18n } from 'vue-i18n'
-  - Judging whether statement is repeated
-  - at top of setup function
+1. import { useI18n } from 'vue-i18n' ✅
+  - Judging whether statement is repeated ✅
 2. const { t } = useI18n()
   - Judging whether statement is repeated
   - below import statement of useI18n
 3. ref(s) => ref(t('xxx'))
-  - if theres no 'ref' imported,import ref
+  - if theres no 'ref' imported,import ref ✅
 4. s => computed(()=>t('xxx'))
-  - if theres no 'computed' imported,import computed
+  - if theres no 'computed' imported,import computed ✅
 */
-class Script extends Visitor {
-  visitProgram(n: Program): Program {
-    let shouldImportI18n = true
-    let shouldImportRef = true
-    let shouldImportComputed = true
-    n.body.forEach((node) => {
-      if (node.type === 'ImportDeclaration') {
-        if (node.source.value === 'vue') {
-          node.specifiers.forEach((node) => {
-            if (
-              node.type === 'ImportSpecifier'
-              && node.imported
-              && node.imported.type === 'Identifier'
-            ) {
-              if (node.imported.value === 'ref')
+function hasImportedMember(
+  node: babelCore.types.ImportDeclaration,
+  name: string,
+) {
+  node.specifiers.forEach((item) => {
+    if (
+      item.type === 'ImportSpecifier'
+      && item.imported.type === 'Identifier'
+      && item.imported.name === name
+    )
+      return true
+  })
+  return false
+}
+export default function(
+  babel: typeof babelCore,
+): babelCore.PluginObj<VisitorState> {
+  const { types: t, template } = babel
+  let i18nImportNode: babelCore.types.ImportDeclaration | null = null
+  let vueImportNode: babelCore.types.ImportDeclaration | null = null
+  let shouldImportI18n = true
+  let shouldImportRef = true
+  let shouldImportComputed = true
+  return {
+    visitor: {
+      Program(path) {
+        const imports = path.node.body.filter(
+          node => node.type === 'ImportDeclaration',
+        )
+        imports.forEach((node) => {
+          if (node.type === 'ImportDeclaration') {
+            if (node.source.value === 'vue-i18n') {
+              i18nImportNode = node
+              if (hasImportedMember(node, 'useI18n'))
+                shouldImportI18n = false
+            }
+            if (node.source.value === 'vue') {
+              vueImportNode = node
+              if (hasImportedMember(node, 'ref'))
                 shouldImportRef = false
-              if (node.imported.value === 'computed')
+              if (hasImportedMember(node, 'computed'))
                 shouldImportComputed = false
             }
-          })
+          }
+        })
+        if (!i18nImportNode) {
+          path.node.body.unshift(
+            t.importDeclaration(
+              [
+                t.importSpecifier(
+                  t.identifier('useI18n'),
+                  t.identifier('useI18n'),
+                ),
+              ],
+              t.stringLiteral('vue-i18n'),
+            ),
+          )
         }
-        if (node.source.value === 'vue-i18n')
-          shouldImportI18n = false
-      }
-    })
-    if (shouldImportI18n) {
-      // Import i18n
-    }
-    if (shouldImportRef) {
-      // import ref
-    }
-    if (shouldImportComputed) {
-      // import computed
-    }
-    return n
+        else if (i18nImportNode && shouldImportI18n) {
+          i18nImportNode.specifiers.push(
+            t.importSpecifier(t.identifier('useI18n'), t.identifier('useI18n')),
+          )
+        }
+        if (!vueImportNode) {
+          path.node.body.unshift(
+            t.importDeclaration(
+              [t.importSpecifier(t.identifier('ref'), t.identifier('ref'))],
+              t.stringLiteral('vue'),
+            ),
+          )
+        }
+        else {
+          if (shouldImportRef)
+            vueImportNode.specifiers.push(t.importSpecifier(t.identifier('ref'), t.identifier('ref')))
+          if (shouldImportComputed)
+            vueImportNode.specifiers.push(t.importSpecifier(t.identifier('computed'), t.identifier('computed')))
+        }
+      },
+    },
   }
 }
-export const ScriptPlugin = (module: Program) =>
-  new Script().visitProgram(module)
