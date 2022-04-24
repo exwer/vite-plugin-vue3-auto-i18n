5b9cfbe4c3e0df7a8c1c7b0cc0a333f8181ba4c4
lbw <574524020@qq.com>
2022-04-24 18:16:21 +0800

    refactor:sort test code

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,46 +3,62 @@ import { start } from '../src/index'
 
 describe('import test', () => {
   test('All absence', async() => {
-    const source = '<script>import { a } from "b"</script>'
-    const result = await start(source)
-    expect(result).toMatchInlineSnapshot(`
-      "<script>import { ref, computed } from \\"vue\\";
-      import { useI18n } from \\"vue-i18n\\";
-      import { a } from \\"b\\";</script>"
-    `)
+    expect(await start(`
+    <script>
+      import { a } from "b"
+    </script>`))
+      .toMatchInlineSnapshot(`
+        "
+            <script>import { ref, computed } from \\"vue\\";
+        import { useI18n } from \\"vue-i18n\\";
+        import { a } from \\"b\\";</script>"
+      `)
   })
 
   test('no ref', async() => {
-    const source
-      = '<script>import {computed} from "vue";import {useI18n} from "vue-i18n"</script>'
-    const result = await start(source)
-    expect(result).toMatchInlineSnapshot(`
-      "<script>import { computed, ref } from \\"vue\\";
-      import { useI18n } from \\"vue-i18n\\";</script>"
+    expect(await start(`
+    <script>
+      import {computed} from "vue";
+      import {useI18n} from "vue-i18n"
+    </script>
+    `)).toMatchInlineSnapshot(`
+      "
+          <script>import { computed, ref } from \\"vue\\";
+      import { useI18n } from \\"vue-i18n\\";</script>
+          "
     `)
   })
 
   test('no computed', async() => {
-    const source
-      = '<script>import {ref} from "vue";import {useI18n} from "vue-i18n"</script>'
-    const result = await start(source)
-    expect(result).toMatchInlineSnapshot(`
-      "<script>import { ref, computed } from \\"vue\\";
-      import { useI18n } from \\"vue-i18n\\";</script>"
+    expect(await start(`
+    <script>
+      import {ref} from "vue";
+      import {useI18n} from "vue-i18n"
+    </script>
+    `)).toMatchInlineSnapshot(`
+      "
+          <script>import { ref, computed } from \\"vue\\";
+      import { useI18n } from \\"vue-i18n\\";</script>
+          "
     `)
   })
 
   test('no useI18n', async() => {
-    const source = '<script>import {ref,computed} from "vue";import {shit} from "vue-i18n"</script>'
-    const result = await start(source)
-    expect(result).toMatchInlineSnapshot(`
-      "<script>import { ref, computed } from \\"vue\\";
-      import { shit, useI18n } from \\"vue-i18n\\";</script>"
-    `)
+    expect(await start(`
+        <script>
+          import {ref,computed} from "vue";import {shit} from "vue-i18n"
+        </script>
+      `))
+      .toMatchInlineSnapshot(`
+        "
+                <script>import { ref, computed } from \\"vue\\";
+        import { shit, useI18n } from \\"vue-i18n\\";</script>
+              "
+      `)
   })
 })
 
-describe('variable test', () => {
+/* describe('variable test', () => {
   test('no useI18n', async() => {
     expect(await start('<script>const a = "xxx"</script>'))
       .toMatchInlineSnapshot()
@@ -52,4 +68,4 @@ describe('variable test', () => {
     expect(await start('<script>const { other } = useI18n()</script>'))
       .toMatchInlineSnapshot()
   })
-})
+}) */
